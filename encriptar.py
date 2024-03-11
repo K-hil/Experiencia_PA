3594c5b6f83ba6127c1bafe5c4b92845e4036ac8
K-hil <105673453+K-hil@users.noreply.github.com>
2024-03-11 14:31:56 +0000

    modificacion encriptacion

--- encriptar.py
+++ encriptar.py
@@ -20,3 +20,28 @@ def encriptar_mensaje(mensaje: str) -> str:
     Retorna:
         str: El mensaje encriptado.
     """
+
+    with open('libro_super_secreto.txt', 'r') as libro:
+        lineas = libro.readlines()
+        mensaje_encriptado = ''
+        for letra in mensaje:
+            if letra not in (' ', '\n'):
+                pos_linea = encontrar_en_lineas(letra, lineas)
+                pos_palabra, pos_letra = encontrar_en_palabras(letra, lineas[pos_linea].split(' '))
+                mensaje_encriptado += f'{pos_linea},{pos_palabra},{pos_letra}'
+            else:
+                mensaje_encriptado += ';'
+    return mensaje_encriptado
+
+
+def encontrar_en_palabras(letra, palabras):
+    for num_palabra, palabra in enumerate(palabras):
+        for c_pos, c in enumerate(palabra):
+            if c == letra:
+                return num_palabra, c_pos
+            
+def encontrar_en_lineas(letra, lineas):
+    for i, linea in enumerate(lineas):
+        if letra in linea:
+            return i
+    return -1 
